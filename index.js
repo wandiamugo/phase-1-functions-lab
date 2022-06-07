4580d5332211cd32424017f88758b60c168871f4
Wandia E Mugo <wwandia173@gmail.com>
2022-06-07 23:11:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+//const destination=43;
+//const start=42;
+function distanceFromHqInBlocks(distance){
+   if (distance>42) {
+    return (distance-42);
+  } else {
+      return (42-distance);
+  }
+}
+
+function distanceFromHqInFeet(distance){
+    return (distanceFromHqInBlocks(distance)*264);
+}
+
+function distanceTravelledInFeet(start,destination){
+    if (start<=destination) {
+        return (destination-start)*264;
+    } else {
+        return (start-destination)*264;
+    }
+    }
+
+function  calculatesFarePrice(start, destination) {
+    let distance=distanceTravelledInFeet(start,destination);
+    if (distance<400) {
+        return (distance*0);
+    } else if (distance>400 && distance<2000) {
+        return (distance-400)*0.02;
+    } else if (distance>2000 && distance<2500) {
+        return (25);
+    } else {
+        return ("cannot travel that far")
+    }
+}
